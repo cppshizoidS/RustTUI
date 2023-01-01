3df83316f77a61c26e859a8c626509fa56825e0a
Cppshizoid <alex.krivoschecov@gmail.com>
2023-01-01 09:14:24 -0800

    Update db.rs

--- src/db.rs
+++ src/db.rs
@@ -10,7 +10,7 @@ impl Database {
 	pub fn new(key: String) -> Result<Database, rusqlite::Error> {
 		let path = dirs::config_dir().unwrap().join("my_passmng");
 		let conn = Connection::open(path)?;
-		// set password to our database. without this passphrase database is not readable
+		// set password to database. without this passphrase database is not readable
 		conn.pragma_update(Some(DatabaseName::Main), "KEY", key)?;
 		let db = Database { conn };
 		db.create_table()?;
